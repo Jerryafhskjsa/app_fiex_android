397aacf068fb20e0fc611a3eac504f0a26836dcf
li xiao <395912555@qq.com>
2022-09-06 22:17:43 +0800

    资金划转页面以及相关弹窗

---
+++ base/src/main/java/com/black/base/view/ChooseWalletControllerWindow.kt
@@ -0,0 +1,143 @@
+package com.black.base.view
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.PaintDrawable
+import android.media.Image
+import android.text.TextUtils
+import android.view.*
+import android.widget.*
+import com.black.base.R
+import skin.support.content.res.SkinCompatResources
+import java.util.*
+
+//换砖钱包类型选择弹窗
+class ChooseWalletControllerWindow<T>(private val activity: Activity, title: String?, private val selectObject: T?, data: List<T>?, private val onReturnListener: OnReturnListener<T>?) : View.OnClickListener, AdapterView.OnItemClickListener {
+    private val COLOR_DEFAULT: Int = SkinCompatResources.getColor(activity, R.color.T1)
+    private val COLOR_SELECT: Int = SkinCompatResources.getColor(activity, R.color.C1)
+    private val COLOR_BG: Int = SkinCompatResources.getColor(activity, R.color.B2)
+    private var density: Float
+    private val popupWindow: PopupWindow
+    private val titleView: TextView
+    private val listView: ListView
+    private val adapter: ChooseWalletListAdapter
+
+    init {
+        val dm = activity.resources.displayMetrics
+        density = dm.density
+        val contentView = LayoutInflater.from(activity).inflate(R.layout.view_wallet_chooser, null)
+        popupWindow = PopupWindow(contentView,
+                dm.widthPixels,
+                WindowManager.LayoutParams.WRAP_CONTENT)
+        popupWindow.isFocusable = true
+        popupWindow.setBackgroundDrawable(PaintDrawable())
+        popupWindow.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED
+        popupWindow.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
+        popupWindow.setOnDismissListener {
+            val lp = activity.window.attributes
+            lp.alpha = 1f
+            activity.window.attributes = lp
+        }
+        titleView = contentView.findViewById(R.id.title)
+        if (TextUtils.isEmpty(title)) {
+            titleView.visibility = View.GONE
+        } else {
+            titleView.visibility = View.VISIBLE
+            titleView.text = title
+        }
+        listView = contentView.findViewById(R.id.list_view)
+        val drawable = ColorDrawable()
+        drawable.color = SkinCompatResources.getColor(activity, R.color.L1_ALPHA60)
+        listView.divider = drawable
+        listView.dividerHeight = 0
+        val backgroundDrawable = ColorDrawable()
+        backgroundDrawable.color = SkinCompatResources.getColor(activity, R.color.B2)
+        listView.background = backgroundDrawable
+        adapter = ChooseWalletListAdapter(activity, data)
+        listView.adapter = adapter
+        listView.onItemClickListener = this
+        //重设listView的高度
+        val params = listView.layoutParams
+        val dataCount = data?.size ?: 0
+        if (dataCount >= 5) {
+            params.height = (60 * 5 * density).toInt()
+        } else {
+            params.height = (60 * dataCount * density).toInt()
+        }
+        listView.layoutParams = params
+        contentView.findViewById<View>(R.id.btn_cancel).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        if (i == R.id.btn_cancel) {
+            dismiss()
+        }
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) { //点击之后回调选择结果
+        onReturnListener?.onReturn(this, adapter.getItem(position))
+        dismiss()
+    }
+
+    fun show() {
+        val lp = activity.window.attributes
+        lp.alpha = 0.6f
+        activity.window.attributes = lp
+        popupWindow.showAtLocation(activity.window.decorView, Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0, 0)
+    }
+
+    fun dismiss() {
+        popupWindow.dismiss()
+    }
+
+    interface OnReturnListener<T> {
+        fun onReturn(window: ChooseWalletControllerWindow<T>, item: T)
+    }
+
+    private inner class ChooseWalletListAdapter(protected var context: Context, data: List<T>?) : BaseAdapter() {
+        protected var inflater: LayoutInflater = LayoutInflater.from(context)
+        private var data: List<T>
+
+        fun setData(data: List<T>?) {
+            this.data = data ?: ArrayList()
+        }
+
+        override fun getCount(): Int {
+            return data.size
+        }
+
+        override fun getItem(position: Int): T {
+            return data[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+            var view = convertView
+            if (view == null) {
+                view = inflater.inflate(R.layout.list_item_wallet_choose, null)
+            }
+            val item = getItem(position)
+            val textView = view?.findViewById<View>(R.id.text) as TextView?
+            var select = view?.findViewById<View>(R.id.img_select) as ImageView?
+            if (item == selectObject) {
+                textView?.setTextColor(COLOR_SELECT)
+                select?.visibility = View.GONE
+            } else {
+                textView?.setTextColor(COLOR_DEFAULT)
+                select?.visibility = View.VISIBLE
+            }
+            textView?.text = item.toString()
+            view?.setBackgroundColor(COLOR_BG)
+            return view
+        }
+
+        init {
+            this.data = data ?: ArrayList()
+        }
+    }
+}
--- wallet/src/main/java/com/black/wallet/activity/LeverPairChooseActivity.kt
+++ wallet/src/main/java/com/black/wallet/activity/LeverPairChooseActivity.kt
@@ -1,73 +1,42 @@
 package com.black.wallet.activity
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.black.base.activity.BaseActionBarActivity
-import com.black.base.adapter.interfaces.OnItemClickListener
-import com.black.base.util.ConstData
-import com.black.base.util.RouterConstData
-import com.black.base.util.RxJavaHelper
-import com.black.base.util.SocketDataContainer
-import com.black.lib.view.SwipeItemLayout
+import com.black.base.util.*
+import com.black.base.view.ChooseCoinControllerWindow
+import com.black.base.view.ChooseWalletControllerWindow
+import com.black.base.view.DeepControllerWindow
 import com.black.router.annotation.Route
-import com.black.wallet.BR
 import com.black.wallet.R
 import com.black.wallet.adapter.LeverPairAdapter
 import com.black.wallet.databinding.ActivityLeverPairChooseBinding
 import io.reactivex.Observable
-import skin.support.content.res.SkinCompatResources
 
 @Route(value = [RouterConstData.LEVER_PAIR_CHOOSE])
-class LeverPairChooseActivity : BaseActionBarActivity(), OnItemClickListener {
+class LeverPairChooseActivity : BaseActionBarActivity(), View.OnClickListener{
     private var binding: ActivityLeverPairChooseBinding? = null
     private var adapter: LeverPairAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_lever_pair_choose)
-
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = RecyclerView.VERTICAL
-        layoutManager.isSmoothScrollbarEnabled = true
-        binding?.recyclerView?.layoutManager = layoutManager
-        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        val drawable = SkinCompatResources.getDrawable(this, R.drawable.divider_list_item_l1)
-        drawable.alpha = (0.6 * 255).toInt()
-        decoration.setDrawable(drawable)
-        binding?.recyclerView?.addItemDecoration(decoration)
-        binding?.recyclerView?.addOnItemTouchListener(SwipeItemLayout.OnSwipeItemTouchListener(this))
-        adapter = LeverPairAdapter(this, BR.listItemLeverPairModel, null)
-        adapter?.setOnItemClickListener(this)
-        binding?.recyclerView?.adapter = adapter
-        binding?.recyclerView?.setEmptyView(binding?.emptyView?.root)
-        binding?.recyclerView?.isNestedScrollingEnabled = false
-        binding?.recyclerView?.setHasFixedSize(true)
-        binding?.recyclerView?.isFocusable = false
-
-        getLeverPair()
+        binding?.relFrom?.setOnClickListener(this)
+        binding?.relTo?.setOnClickListener(this)
+        binding?.relChoose?.setOnClickListener(this)
+        var actionBarRecord: ImageButton? = binding?.root?.findViewById(R.id.img_action_bar_right)
+        actionBarRecord?.visibility = View.VISIBLE
     }
 
     override fun isStatusBarDark(): Boolean {
         return !super.isStatusBarDark()
     }
 
-    override fun getTitleText(): String {
-        return "选择账户"
-    }
 
-    override fun onItemClick(recyclerView: RecyclerView?, view: View, position: Int, item: Any?) {
-        val pair = adapter?.getItem(position)
-        pair?.run {
-            val intent = Intent()
-            intent.putExtra(ConstData.PAIR, pair)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        }
+    override fun getTitleText(): String {
+        return getString(R.string.asset_transfer)
     }
 
     private fun getLeverPair() {
@@ -78,5 +47,76 @@ class LeverPairChooseActivity : BaseActionBarActivity(), OnItemClickListener {
                     adapter?.notifyDataSetChanged()
                 }
                 .run { }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.rel_from -> {
+                showWalletChooseDialog()
+            }
+            R.id.rel_to ->{
+
+            }
+            R.id.rel_choose ->{
+                showCoinChooseDialog()
+            }
+        }
+    }
+
+    private fun showWalletChooseDialog(){
+        var fromWallet = "现货账户"
+        var toWallet = "合约账户"
+        var wallList = ArrayList<Wallet?>()
+        var wallet1 = Wallet()
+        wallet1.name = "现货账户"
+        var wallet2 = Wallet()
+        wallet2.name = "合约账户"
+        var wallet3 = Wallet()
+        wallet3.name = "理财账户"
+        var wallet4 = Wallet()
+        wallet4.name = "钱包账户"
+        wallList.add(wallet1)
+        wallList.add(wallet2)
+        wallList.add(wallet3)
+        wallList.add(wallet4)
+        ChooseWalletControllerWindow(mContext as Activity, getString(R.string.select_wallet), wallet1,
+            wallList,
+            object : ChooseWalletControllerWindow.OnReturnListener<Wallet?> {
+                override fun onReturn(window: ChooseWalletControllerWindow<Wallet?>, item: Wallet?) {
+
+                }
+
+            }).show()
+    }
+
+    private fun showCoinChooseDialog(){
+        var fromWallet = "现货账户"
+        var toWallet = "合约账户"
+        var wallList = ArrayList<Wallet?>()
+        var wallet1 = Wallet()
+        wallet1.name = "现货账户"
+        var wallet2 = Wallet()
+        wallet2.name = "合约账户"
+        var wallet3 = Wallet()
+        wallet3.name = "理财账户"
+        var wallet4 = Wallet()
+        wallet4.name = "钱包账户"
+        wallList.add(wallet1)
+        wallList.add(wallet2)
+        wallList.add(wallet3)
+        wallList.add(wallet4)
+        ChooseCoinControllerWindow(mContext as Activity, getString(R.string.select_wallet), wallet1,
+            wallList,
+            object : ChooseCoinControllerWindow.OnReturnListener<Wallet?> {
+                override fun onReturn(window: ChooseCoinControllerWindow<Wallet?>, item: Wallet?) {
+
+                }
+
+            }).show()
+    }
+
+    inner class Wallet{
+        var name:String? = null
+        var selected:Boolean? = null
     }
 }
