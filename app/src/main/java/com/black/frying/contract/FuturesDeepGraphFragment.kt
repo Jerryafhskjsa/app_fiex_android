a4f4e9dcfd2d19dd9f7f30990e56df1712ca8bd9
zxadmin <zhuxiao@g7.com.cn>
2023-04-19 18:09:43 +0800

    [feat] 期货深度图

--- app/src/main/java/com/black/frying/contract/FuturesDeepGraphFragment.kt
+++ app/src/main/java/com/black/frying/contract/FuturesDeepGraphFragment.kt
@@ -1,19 +1,27 @@
 package com.black.frying.contract
 
-import androidx.lifecycle.ViewModelProvider
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.black.base.model.socket.Deep
+import com.black.base.model.socket.PairStatus
+import com.black.base.model.socket.TradeOrder
+import com.black.base.util.ConstData
+import com.black.base.util.FryingUtil
+import com.black.base.util.SocketDataContainer
+import com.black.base.view.DeepControllerWindow
 import com.black.frying.contract.state.FutureGlobalStateViewModel
 import com.black.frying.contract.viewmodel.FuturesTransactionInfoDisplayViewModel
-import com.black.frying.view.ContractDeepViewBinding
 import com.black.lib.view.ProgressDrawable
+import com.black.util.NumberUtil
 import com.fbsex.exchange.R
-import com.fbsex.exchange.databinding.FragmentLayoutFuturesDeepGraphDisplayBinding
 import com.fbsex.exchange.databinding.FuturesLayoutDeepGraphBinding
 import com.zhy.adapter.recyclerview.CommonAdapter
 import com.zhy.adapter.recyclerview.base.ViewHolder
@@ -25,16 +33,18 @@ class FuturesDeepGraphFragment : Fragment() {
         SELL,
         BUY;
 
-      public  fun ifDefault() = this ==DEFAULT
+        public fun ifDefault() = this == DEFAULT
     }
+
+    val maxDoubleShowCount = 5
 
     companion object {
         const val TAG = "FuturesDeepGraphFragment"
         fun newInstance() = FuturesDeepGraphFragment()
     }
 
-    private  val viewModel: FuturesTransactionInfoDisplayViewModel by lazy {
-      ViewModelProvider(this).get(FuturesTransactionInfoDisplayViewModel::class.java)
+    private val viewModel: FuturesTransactionInfoDisplayViewModel by lazy {
+        ViewModelProvider(this).get(FuturesTransactionInfoDisplayViewModel::class.java)
     }
     private val globalViewModel: FutureGlobalStateViewModel by lazy {
         ViewModelProvider(
@@ -68,26 +78,63 @@ class FuturesDeepGraphFragment : Fragment() {
 
         setupBindData()
     }
-    private val colorTransparent: Int by lazy {  SkinCompatResources.getColor(context, R.color.transparent) }
-    private val colorT7A10: Int by lazy {  SkinCompatResources.getColor(context, R.color.T7_ALPHA10)}
-    private val colorT5A10: Int by lazy { SkinCompatResources.getColor(context, R.color.T5_ALPHA10)}
+
+    val nullAmount by lazy { context!!.resources.getString(R.string.number_default) }
+
+    private val colorTransparent: Int by lazy {
+        SkinCompatResources.getColor(
+            context,
+            R.color.transparent
+        )
+    }
+    private val colorT7A10: Int by lazy {
+        SkinCompatResources.getColor(
+            context,
+            R.color.T7_ALPHA10
+        )
+    }
+    private val colorT5A10: Int by lazy {
+        SkinCompatResources.getColor(
+            context,
+            R.color.T5_ALPHA10
+        )
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        viewModel.init(context!!)
+    }
 
     private fun setupBindView() {
         binding.apply {
             //set rv
             rvUpList.apply {
                 layoutManager = LinearLayoutManager(context)
-                adapter = object:CommonAdapter<Array<String?>?>(context,R.layout.futures_layout_deep_graph_item,viewModel.sellList){
-                    override fun convert(holder: ViewHolder?, t: Array<String?>?, position: Int) {
-                        holder?.getView<ViewGroup>(R.id.handicap_buy_layout_01)?.let {
-                            background = ProgressDrawable(colorT5A10, colorTransparent, ProgressDrawable.RIGHT)
+                adapter = object : CommonAdapter<TradeOrder>(
+                    context,
+                    R.layout.futures_layout_deep_graph_item,
+                    viewModel.sellList
+                ) {
+                    override fun convert(
+                        holder: ViewHolder?,
+                        tradeOrder: TradeOrder,
+                        position: Int
+                    ) {
+                        holder?.getView<ViewGroup>(R.id.rl_bg)?.let {
+                            it.background = ProgressDrawable(
+                                colorT5A10,
+                                colorTransparent,
+                                ProgressDrawable.RIGHT
+                            )
+                            setOrderBg(it,tradeOrder)
                         }
 
-                        holder?.getView<TextView>(R.id.price_buy_01)?.let {
-                            it.text = t?.get(0)
+                        holder?.getView<TextView>(R.id.tv_price)?.let {
+                            it.text =tradeOrder?.formattedPrice
+                            it.setTextColor(it.context.getColor(R.color.T5))
                         }
-                        holder?.getView<TextView>(R.id.count_buy_01)?.let {
-                            it.text = t?.get(1)
+                        holder?.getView<TextView>(R.id.tv_amount)?.let {
+                            it.text =tradeOrder?.exchangeAmountFormat
                         }
                     }
                 }
@@ -95,17 +142,31 @@ class FuturesDeepGraphFragment : Fragment() {
 
             rvBtmList.apply {
                 layoutManager = LinearLayoutManager(context)
-                adapter = object:CommonAdapter<Array<String?>?>(context,R.layout.futures_layout_deep_graph_item,viewModel.buyList){
-                    override fun convert(holder: ViewHolder?, t: Array<String?>?, position: Int) {
-                        holder?.getView<ViewGroup>(R.id.handicap_buy_layout_01)?.let {
-                            background = ProgressDrawable(colorT7A10, colorTransparent, ProgressDrawable.RIGHT)
+                adapter = object : CommonAdapter<TradeOrder>(
+                    context,
+                    R.layout.futures_layout_deep_graph_item,
+                    viewModel.buyList
+                ) {
+                    override fun convert(
+                        holder: ViewHolder?,
+                        tradeOrder: TradeOrder,
+                        position: Int
+                    ) {
+                        holder?.getView<ViewGroup>(R.id.rl_bg)?.let {
+                            it.background = ProgressDrawable(
+                                colorT7A10,
+                                colorTransparent,
+                                ProgressDrawable.RIGHT
+                            )
+
+                            setOrderBg(it,tradeOrder)
                         }
 
-                        holder?.getView<TextView>(R.id.price_buy_01)?.let {
-                            it.text = t?.get(0)
+                        holder?.getView<TextView>(R.id.tv_price)?.let {
+                            it.text =tradeOrder?.formattedPrice
                         }
-                        holder?.getView<TextView>(R.id.count_buy_01)?.let {
-                            it.text = t?.get(1)
+                        holder?.getView<TextView>(R.id.tv_amount)?.let {
+                            it.text =tradeOrder?.exchangeAmountFormat
                         }
                     }
                 }
@@ -114,27 +175,61 @@ class FuturesDeepGraphFragment : Fragment() {
             displayType.setOnClickListener {
                 viewModel.clickShowMode()
             }
+
+            deep.setOnClickListener {
+                if (!viewModel.getPrecisionList().isNullOrEmpty()) {
+                    DeepControllerWindow<Deep>(requireActivity(), null, viewModel.getPrecisionDeep(viewModel.getPrecision()), viewModel.getPrecisionList() as List<Deep>,
+                        object : DeepControllerWindow.OnReturnListener<Deep> {
+                            override fun onReturn(window: DeepControllerWindow<Deep>, item: Deep) {
+                                item.precision?.let { viewModel.setPrecision(it) }
+
+                                refreshDeepView()
+                            }
+                        }).show()
+                }
+            }
         }
     }
-
-    val maxDoubleShowCount = 5
-
+    private fun refreshDeepView() {
+        var deep = viewModel!!.getPrecisionDeep(viewModel!!.getPrecision())
+        binding!!.deep.setText(
+            getString(
+                R.string.point_count,
+                deep?.deep ?: ""
+            )
+        )
+    }
     private fun setupBindData() {
-       val showCount =  if(viewModel.showMode.value!!.ifDefault()){maxDoubleShowCount}else maxDoubleShowCount*2
-        globalViewModel.tradeOrderDepthLiveData.observe(viewLifecycleOwner){
-            it.a?.let {
-                viewModel.sellList.apply {
-                    clear()
-                    addAll(it.take(showCount).toList())
+        globalViewModel.tradeOrderDepthLiveData.observe(viewLifecycleOwner) {
+            val showCount = if (viewModel.showMode.value!!.ifDefault()) {
+                maxDoubleShowCount
+            } else maxDoubleShowCount * 2
+            // trans
+            val parseOrderDepthData =
+                SocketDataContainer.parseOrderDepthData(context, ConstData.DEPTH_FUTURE_TYPE, it.apply {
+                    this.a = a?.takeLast(showCount)?.toTypedArray()
+                    this.b = b?.take(showCount)?.toTypedArray()
+                })
+            parseOrderDepthData?.apply {
+                bidOrderList?.let {
+                    viewModel.buyList.apply {
+                        clear()
+                        addAll(it.toList())
+                        FryingUtil.computeTradeOrderWeightPercent(this, showCount)
+                        formatShowPrice()
+                    }
+                    binding.rvBtmList.adapter?.notifyDataSetChanged()
                 }
-                binding.rvUpList?.adapter?.notifyDataSetChanged()
-            }
-            it.b?.let {
-                viewModel.buyList.apply {
-                    clear()
-                    addAll(it.take(showCount).toList())
+
+                askOrderList?.let {
+                    viewModel.sellList.apply {
+                        clear()
+                        FryingUtil.computeTradeOrderWeightPercent(it, showCount)
+                        addAll(it.reversed().toList())
+                        formatShowPrice()
+                    }
+                    binding.rvUpList.adapter?.notifyDataSetChanged()
                 }
-                binding.rvBtmList?.adapter?.notifyDataSetChanged()
             }
         }
 
@@ -159,7 +254,7 @@ class FuturesDeepGraphFragment : Fragment() {
                         displayType.setImageDrawable(
                             SkinCompatResources.getDrawable(
                                 context,
-                                R.drawable.icon_trade_deep_02
+                                R.drawable.icon_trade_deep_03
                             )
                         )
                     }
@@ -171,13 +266,40 @@ class FuturesDeepGraphFragment : Fragment() {
                         displayType.setImageDrawable(
                             SkinCompatResources.getDrawable(
                                 context,
-                                R.drawable.icon_trade_deep_03
+                                R.drawable.icon_trade_deep_02
                             )
                         )
                     }
                 }
                 else -> {}
             }
+        }
+    }
+    private fun setOrderBg(layout: View?, tradeOrder: TradeOrder?) {
+        if (layout == null) {
+            return
+        }
+        val background = layout.background
+        if (background is ProgressDrawable) {
+            val progress: Double = tradeOrder?.weightPercent ?: 0.toDouble()
+            background.setProgress(progress)
+        }
+    }
+    private fun ArrayList<TradeOrder?>.formatShowPrice() {
+        forEach {
+            it?.formattedPrice =
+                if (it == null) nullAmount else NumberUtil.formatNumberNoGroup(
+                    it.price,
+                    viewModel.currentPairStatus.precision,
+                    viewModel.currentPairStatus.precision
+                )
+            it?.exchangeAmountFormat =
+                if (it == null) nullAmount else NumberUtil.formatNumberDynamicScaleNoGroup(
+                    it.exchangeAmount,
+                    10,
+                    viewModel.getAmountLength(),
+                    viewModel.getAmountLength()
+                )
         }
     }
 
--- app/src/main/java/com/black/frying/contract/viewmodel/FuturesTransactionInfoDisplayViewModel.kt
+++ app/src/main/java/com/black/frying/contract/viewmodel/FuturesTransactionInfoDisplayViewModel.kt
@@ -1,28 +1,151 @@
 package com.black.frying.contract.viewmodel
 
+import android.content.Context
+import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.black.base.activity.BaseActionBarActivity
+import com.black.base.model.socket.Deep
+import com.black.base.model.socket.PairStatus
+import com.black.base.model.socket.TradeOrder
+import com.black.base.util.ConstData
+import com.black.base.util.CookieUtil
+import com.black.base.util.SocketDataContainer
 import com.black.frying.contract.FuturesDeepGraphFragment
+import com.black.util.CommonUtil
+import com.fbsex.exchange.R
 
 class FuturesTransactionInfoDisplayViewModel : ViewModel() {
+    lateinit var mContext: Context
+    private var coinType: String? = null
+    private var pairSet: String? = null
+    fun init(context: Context) {
+        mContext = context
+        initPairStatus()
+    }
+
     fun clickShowMode() {
         val maxIndex = FuturesDeepGraphFragment.ShowMode.values().size - 1
         var ordinal = showMode.value!!.ordinal
-        if(ordinal>=maxIndex){
-            ordinal=0
-        }else{
-            ordinal+=1
+        if (ordinal >= maxIndex) {
+            ordinal = 0
+        } else {
+            ordinal += 1
         }
-        val nV =  Math.min(maxIndex,ordinal)
+        val nV = Math.min(maxIndex, ordinal)
         showMode.postValue(FuturesDeepGraphFragment.ShowMode.values()[nV])
     }
 
-    val showMode = MutableLiveData<FuturesDeepGraphFragment.ShowMode>(FuturesDeepGraphFragment.ShowMode.DEFAULT)
+    val showMode =
+        MutableLiveData<FuturesDeepGraphFragment.ShowMode>(FuturesDeepGraphFragment.ShowMode.DEFAULT)
 
-    val sellList: ArrayList<Array<String?>?> = ArrayList<Array<String?>?>()
-    val buyList: ArrayList<Array<String?>?> = ArrayList<Array<String?>?>()
+    val sellList: ArrayList<TradeOrder?> = ArrayList()
+    val buyList: ArrayList<TradeOrder?> = ArrayList()
+     var currentPairStatus = PairStatus()
+
+    private fun initPairStatus() {
+        val onResumeTodo = Runnable {
+            val currentPair = getLastPair()
+            if (!TextUtils.equals(currentPair, currentPairStatus.pair)) {
+                currentPairStatus = PairStatus()
+                currentPairStatus.pair = (currentPair)
+                initPairCoinSet()
+                currentPairStatus.supportingPrecisionList = null
+                currentPairStatus.precision = ConstData.DEFAULT_PRECISION
+            }
+            getCurrentPairStatus(currentPairStatus.pair)
+        }
+        currentPairStatus.pair = (getLastPair())
+        initPairCoinSet()
+        currentPairStatus.supportingPrecisionList = null
+        currentPairStatus.precision = ConstData.DEFAULT_PRECISION
+        if (currentPairStatus.pair == null) {
+//            SocketDataContainer.initAllFutureUsdtPairStatusData(context)
+        } else {
+            onResumeTodo.run()
+        }
+    }
+    private fun initPairCoinSet() {
+        currentPairStatus.pair?.run {
+            val arr: Array<String>? = currentPairStatus.pair?.split("_")?.toTypedArray()
+            if (arr != null && arr.size > 1) {
+                coinType = arr[0]
+                pairSet = arr[1]
+            }
+        }
+    }
+
+    fun getCurrentPairStatus(pair: String?) {
+        currentPairStatus.pair = (pair)
+        initPairCoinSet()
+        val pairStatus: PairStatus? = SocketDataContainer.getPairStatusSync(
+            mContext,
+            ConstData.PairStatusType.FUTURE_ALL,
+            pair
+        )
+        if (pairStatus != null) {
+            currentPairStatus = pairStatus
+            initPairCoinSet()
+        } else {
+//            SocketDataContainer.initAllFutureUsdtPairStatusData(context)
+        }
+        resetPairStatus(pairStatus)
+    }
+
+    private fun resetPairStatus(pairStatus: PairStatus?) {
+        if (pairStatus == null) {
+            return
+        }
+        currentPairStatus.currentPrice = (pairStatus.currentPrice)
+        currentPairStatus.setCurrentPriceCNY(
+            pairStatus.currentPriceCNY,
+            mContext.getString(R.string.number_default)
+        )
+        currentPairStatus.maxPrice = (pairStatus.maxPrice)
+        currentPairStatus.minPrice = (pairStatus.minPrice)
+        currentPairStatus.priceChangeSinceToday = (pairStatus.priceChangeSinceToday)
+        currentPairStatus.totalAmount = (pairStatus.totalAmount)
+        if (pairStatus.pair != null) {
+            currentPairStatus.pair = (pairStatus.pair)
+        }
+    }
+    private fun getLastPair(): String? {
+        var pair = CookieUtil.getCurrentFutureUPair(mContext)
+        if (pair == null) {
+            val allPair = SocketDataContainer.getAllPair(mContext, ConstData.PairStatusType.FUTURE_U)
+            if (allPair != null) {
+                pair = CommonUtil.getItemFromList(allPair, 0)
+            }
+        }
+        return pair
+    }
 
 
+    fun getPrecisionDeep(precision: Int?): Deep? {
+        var deepList = currentPairStatus.supportingPrecisionList
+        if (deepList != null) {
+            for (deep in deepList) {
+                if (precision == deep.precision) {
+                    return deep
+                }
+            }
+        }
+        return null
+    }
+    fun getPrecisionList(): ArrayList<Deep>? {
+        return currentPairStatus.supportingPrecisionList
+    }
+
+    fun getAmountLength(): Int {
+        return currentPairStatus.amountPrecision ?: 4
+    }
+    fun setPrecision(precision: Int) {
+        currentPairStatus.precision = precision
+    }
+
+    fun getPrecision(): Int? {
+        return currentPairStatus.precision
+    }
 }
 
 
